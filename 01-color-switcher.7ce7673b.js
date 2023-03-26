716fec6361a26caae3d81f3df78ce54aa54d84b1
SzymonSliwa <114831810+SzymonSliwa@users.noreply.github.com>
2023-03-26 19:20:20 +0000

    Deploying to gh-pages from @ SzymonSliwa/goit-js-hw-09@91e863400b8bc3fc153e34e5d64631e18ce6e873 🚀

---
+++ 01-color-switcher.7ce7673b.js
@@ -0,0 +1,2 @@
+console.log.bind();const e=document.querySelector("body"),t=document.querySelector("[data-start]"),o=document.querySelector("[data-stop]");let l=null;t.addEventListener("click",(()=>{l=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),o.addEventListener("click",(()=>{clearInterval(l),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.7ce7673b.js.map
